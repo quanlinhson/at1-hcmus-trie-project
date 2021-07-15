734cda39d712b5faa6e601aeaa5273c7ebee4fcb
TungAlter <81786216+TungAlter@users.noreply.github.com>
2021-07-15 16:45:29 +0700

    CheckEmpty,Count,Search functions

--- trie.cpp
+++ trie.cpp
@@ -3,3 +3,50 @@
 TrieNode::TrieNode(){
     for (unsigned short int i = 0; i<26; i++) child[i]=NULL;
 }
+bool Search_Item(TrieNode* Root, string Key)
+{
+	TrieNode* Temp = Root;
+
+	for (int i = 0; i < Key.length(); i++)
+	{
+		int index = Key[i] - 'a';
+		if (!Temp->child[index])
+		{
+		   return false;
+		}
+		Temp = Temp->child[index];
+	}
+
+	return (Temp->End);
+}
+int Count_Item(TrieNode* Root)
+{
+	int Count = 0;
+
+	if (Root->End)
+	{
+		Count++;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		if (Root->child[i])
+		{
+			Count += Count_Item(Root->child[i]);
+		}
+	}
+
+	return Count;
+}
+bool Check_Is_Empty(TrieNode* Root)
+{
+	TrieNode* Temp = Root;
+	for (int i = 0; i < 26; i++)
+	{
+		if (Temp->child[i])
+		{
+			return false;
+		}
+	}
+	return true;
+
+}
