a5396fc3c46c1753aa04b188719f3dfe21e2b521
quanlinhson <79523507+quanlinhson@users.noreply.github.com>
2021-07-18 16:34:42 +0700

    Update TrieNode* BuildTrie

--- trie.hpp
+++ trie.hpp
@@ -13,3 +13,4 @@ int countItem(TrieNode* Root);
 bool isEmpty(TrieNode* Root);
 void insertItem(TrieNode* root, string key);
 TrieNode* removeItem(TrieNode* root, string key, int depth = 0);
+TrieNode* BuildTrie(TrieNode* root,istream &input, string *keys, int &n);
