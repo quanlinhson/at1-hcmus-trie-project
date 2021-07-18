9717a3f84633c2ed53314a999400e4f4ee138dd5
quanlinhson <79523507+quanlinhson@users.noreply.github.com>
2021-07-18 17:45:02 +0700

    Fix removeItem, Update removeAll, BuildTrie

--- trie.hpp
+++ trie.hpp
@@ -12,4 +12,6 @@ bool searchItem(TrieNode* Root, string Key);
 int countItem(TrieNode* Root);
 bool isEmpty(TrieNode* Root);
 void insertItem(TrieNode* root, string key);
-TrieNode* removeItem(TrieNode* root, string key, int depth = 0);
+TrieNode* removeItem(TrieNode *&root, string key, int depth = 0);
+TrieNode* removeAll(TrieNode *&root,string a[],int n);
+TrieNode* BuildTrie(TrieNode* root, istream &input, string *&keys, int &n);
