ab19539e05f8bdd39494bc3339cec3b4abccfdde
quanlinhson <79523507+quanlinhson@users.noreply.github.com>
2021-07-18 10:30:43 +0700

    Update trie.hpp

--- trie.hpp
+++ trie.hpp
@@ -13,3 +13,4 @@ int countItem(TrieNode* Root);
 bool isEmpty(TrieNode* Root);
 void insertItem(TrieNode* root, string key);
 TrieNode* removeItem(TrieNode* root, string key, int depth = 0);
+TrieNode* removeAll(TrieNode *root);
